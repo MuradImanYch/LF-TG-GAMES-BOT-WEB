e6a0d259c04a8b6e3c00c329c3baa9e218fdcd32
MuradImanYch <mura.imanov@gmail.com>
2026-04-19 12:07:35 +0400

    [ADD] CORS

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./utils/swagger/swagger');
 const users = require('./routes/users');
 const quiz = require('./routes/quiz');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
