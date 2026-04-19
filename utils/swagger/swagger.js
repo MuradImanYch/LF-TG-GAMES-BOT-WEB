804254a00e7d83b4f5f33f6e3020682f51c8b5d1
MuradImanYch <mura.imanov@gmail.com>
2026-04-19 12:24:08 +0400

    [ADD] .env: SERVER_URL

--- utils/swagger/swagger.js
+++ utils/swagger/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:8080',
+        url: process.env.SERVER_URL || 'http://localhost:8080',
         description: 'Local server',
       },
     ],
